68580da665f633b38adfac0c4a9cb1d38306f5e1
manuelJung <manuel.jung@emmos.de>
2017-04-15 01:15:34 +0200

    added updaters to loginHandler (comment)

--- src/reducer/loginHandler.js
+++ src/reducer/loginHandler.js
@@ -2,11 +2,17 @@ import * as t from '../actionTypes'
 
 
 
-const defaultState = {
+export const defaultState = {
   error       : null,
   isFetching  : false,
   fetchFailed : false,
 }
+
+// const updaters = {
+//   waitForResponse: (state) => ({ ...state, isFetching: true, fetchFailed: false, error: null }),
+//   setResponseError: (state, error) => ({ ...state, isFetching: false, fetchFailed: true, error}),
+//   setResponseSuccess: (state) => ({ ...state, isFetching: false })
+// }
 
 export default function reducer (state = defaultState, action) {
   switch (action.type) {
@@ -14,6 +20,8 @@ export default function reducer (state = defaultState, action) {
     case t.FETCH_LOGIN_FAILURE: return { ...state, isFetching: false, fetchFailed: true, error: action.payload }
     case t.FETCH_LOGIN_SUCCESS: return { ...state, isFetching: false }
     
+    // case t.FETCH_LOGIN_REQUEST: return updaters.waitForResponse(state)
+
     default:
       return state
   }
