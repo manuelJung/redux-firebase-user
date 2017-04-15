bf843618f57f9bb11fdafd21709f7193cf9f073f
manuelJung <manuel.jung@emmos.de>
2017-04-15 20:17:07 +0200

    build

--- build/hocs/withLoginRequest.js
+++ build/hocs/withLoginRequest.js
@@ -27,25 +27,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-// import { connect } from 'react-redux'
-
-
-// const mapStateToProps = state => ({
-//   loginRequest: {
-//     isFetching  : isFetchingLogin(state),
-//     fetchFailed : loginFetchFailed(state),
-//     fetchError  : getLoginFetchError(state)
-//   }
-// })
-
-// const mapDispatchToProps = dispatch => ({
-//   loginRequestActions: {
-//     login: (username, password) => dispatch(login(username, password)),
-//     loginWithGoogle: (options) => dispatch(loginWithGoogle(options))
-//   }
-// })
-
-// export default (BaseComponent) => connect(mapStateToProps, mapDispatchToProps)(BaseComponent)
 
 exports.default = function (BaseComponent) {
   var _class, _temp2;
--- build/hocs/withUser.js
+++ build/hocs/withUser.js
@@ -25,21 +25,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-// import { connect } from 'react-redux'
-
-
-// const mapStateToProps = state => ({
-//   user: {
-//     isLoggedIn: isLoggedIn(state),
-//     data: getUser(state)
-//   }
-// })
-
-// const mapDispatchToProps = {
-// }
-
-// export default (BaseComponent) => connect(mapStateToProps, mapDispatchToProps)(BaseComponent)
-
 
 exports.default = function (BaseComponent) {
   var _class, _temp2;
